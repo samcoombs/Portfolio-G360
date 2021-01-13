670dccf05f40e98d71d4d223436df84513699877
samcoombs <76431238+samcoombs@users.noreply.github.com>
2021-01-13 11:50:56 +0000

    JS update

--- script.js
+++ script.js
@@ -1,55 +1,57 @@
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnOpenModal = document.querySelector('.show-modal');
-const btnCloseModal = document.querySelector('.close-modal')
-const html = document.querySelector('html');
+"use strict";
 
-const navAbout = document.querySelector('.navigation__link--about');
-const navDestinations = document.querySelector('.navigation__link--destinations')
-const secAbout = document.getElementById('about');
-const secDestinations = document.getElementById('destinations')
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnOpenModal = document.querySelector(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const html = document.querySelector("html");
 
-const burgerButton = document.querySelector('.burger-nav__button');
-const burgerIcon = document.querySelector('.scroll');
-const header = document.querySelector('.header');
+const navAbout = document.querySelector(".navigation__link--about");
+const navDestinations = document.querySelector(
+  ".navigation__link--destinations"
+);
+const secAbout = document.getElementById("about");
+const secDestinations = document.getElementById("destinations");
 
+const burgerButton = document.querySelector(".burger-nav__button");
+const burgerIcon = document.querySelector(".scroll");
+const header = document.querySelector(".header");
 
 // OFFERS MODAL WINDOWS
-const closeModal = function(e) {
-    e.preventDefault();
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-    html.classList.remove('static');
-}
-const openModal = function(e) {
-    e.preventDefault();
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-    html.classList.add('static');
-}
+const closeModal = function (e) {
+  e.preventDefault();
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  html.classList.remove("static");
+};
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  html.classList.add("static");
+};
 
-btnOpenModal.addEventListener('click', openModal);
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal)
+btnOpenModal.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function(e) {
-
-    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
-    }
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
 });
 
 // SMOOTH SCROLLING
 
-navAbout.addEventListener('click', function(e) {
-    e.preventDefault();
+navAbout.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    secAbout.scrollIntoView({ behavior: 'smooth' })
+  secAbout.scrollIntoView({ behavior: "smooth" });
 });
 
-navDestinations.addEventListener('click', function(e) {
-    e.preventDefault() ;
-    secDestinations.scrollIntoView({ behavior: 'smooth'})
+navDestinations.addEventListener("click", function (e) {
+  e.preventDefault();
+  secDestinations.scrollIntoView({ behavior: "smooth" });
 });
 
 // BURGER NAV CHANGE
@@ -60,31 +62,23 @@ navDestinations.addEventListener('click', function(e) {
 
 const burgerHeight = burgerButton.getBoundingClientRect().height;
 
-
 const changeBurger = function (entries) {
-    const [entry] = entries;
-    
-    if(!entry.isIntersecting) {
-        // burgerButton.style.backgroundColor = 'white';
-        burgerButton.classList.add('btn-scroll');
-        burgerIcon.classList.add('scroll');
-    }
-    else {
-        // burgerButton.style.backgroundColor = 'transparent';
-        burgerButton.classList.remove('btn-scroll');
-        burgerIcon.classList.remove('scroll')
-    }
-}
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    // burgerButton.style.backgroundColor = 'white';
+    burgerButton.classList.add("btn-scroll");
+    burgerIcon.classList.add("scroll");
+  } else {
+    // burgerButton.style.backgroundColor = 'transparent';
+    burgerButton.classList.remove("btn-scroll");
+    burgerIcon.classList.remove("scroll");
+  }
+};
 
 const headerObserver = new IntersectionObserver(changeBurger, {
-    root: null,
-    threshold: 0,
-    rootMargin: `-${burgerHeight}px`
+  root: null,
+  threshold: 0,
+  rootMargin: `-${burgerHeight}px`,
 });
 headerObserver.observe(header);
-
-
-
-
-
-
